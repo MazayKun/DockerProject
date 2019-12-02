8bce55659ad9a303bcd6ffce6b05e1dd1f5b9fab
MazayKun <kir99916@gmail.com>
2019-12-02 15:11:36 +0300

    git status

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+from fabric import Connection, Config
+
+config = Config(overrides={'sudo': {'password': '123'} , 'Password' : '123'})
+connect = Connection ("192.168.1.51", config = config)
+
+try:
+    connect.run("git clone https://github.com/MazayKun/DockerProject.git")
+except BaseException:
+    print("already cloned")
+
+connect.sudo("docker-compose -f DockerProject/docker-compose.yml up")
+
+connect.close()
